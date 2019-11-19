9d2670ec9cbc2e805cb5f95e260088f8c1850fc4
abbluiz <unt@labb.email>
2019-11-19 18:23:49 -0500

    Remove privacytools.io tracker

--- assets/js/main.js
+++ assets/js/main.js
@@ -67,23 +67,3 @@ function changeColorScheme() {
     localStorage.setItem("colorScheme", "dark");
   }
 }
-
-
-// Matomo
-var _paq = window._paq || [];
-/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-_paq.push(["trackPageView"]);
-_paq.push(["enableLinkTracking"]);
-(function() {
-  var u = "https://stats.privacytools.io/";
-  _paq.push(["setTrackerUrl", u + "matomo.php"]);
-  _paq.push(["setSiteId", "1"]);
-  var d = document,
-    g = d.createElement("script"),
-    s = d.getElementsByTagName("script")[0];
-  g.type = "text/javascript";
-  g.async = true;
-  g.defer = true;
-  g.src = u + "matomo.js";
-  s.parentNode.insertBefore(g, s);
-})();
